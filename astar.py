bb44e679f9bde3e76e828a22fa4225616813d6f1
Hanne Gunby <hanngu@stud.ntnu.no>
2012-09-26 20:29:11 +0200

    Made it work, with reasonable runtime.

--- astar.py
+++ astar.py
@@ -1,42 +1,90 @@
 
+import heapq
+import itertools
+
+fraction = 0.5
 
 class Node:
 
-	neighbours = []
+	def __init__(self, parent):
+		self.parent = parent
+		self.numbers = ['1','2','3','4','5','6','7','8','9']
+		self.neighbours = []
+		self.length = 0
+		self.parent = None 
 
-	distance = 0
+		if parent != None:
+			self.length = parent.length + 1
+		else:
+			self.length = 0
+
 	def __lt__(self, other): #Less than. 
-		return self.distance < other.distance
+		return self.h() < other.h()
+
+	def f(self):
+		return g() + h()
+
+	def h(self):
+		return 1
+
+	def g(self):
+		return length
+
+	def __repr__(self):
+		return ''.join(self.numbers)
+
+def isCorrectFraction(node):
+	number = ''.join(node.numbers)
+	numerator = float(number[0:4])
+	denominator = float(number[4:9])
+	thisFraction = numerator/denominator
+
+	if abs(thisFraction - fraction) < 0.001:
+		return True
+	else:
+		return False
 
 
-class Path(list):
-	nodes = []
-	def __lt__ (self, other):
-		return self.nodes[-1] < other.nodes[-1] #Compare two last nodes
-	
-	def __getslice__(self, i, j):
-		return Path(nodes.__getslice__(i, j))
-
-def findPath(startnode, endnode):
+def findPath(startnode, isSolution):
+	print 'er i findPath'
 	visitedNodes = []
-	toVisit = [[]]
+	toVisit = []
 	currentNode = startnode
-	currentPath = [startnode]
 
 	while True:
 		visitedNodes.append(currentNode)
 		for node in currentNode.neighbours:
 			if node not in visitedNodes:
-				newPath = copy.deepcopy(currentPath)
-				newPath.append(node) 
-				toVisit.append(newPath)
-		sort(toVisit)			
-		if len(currentPath) == 0:
+				heapq.heappush(toVisit, node) #pushing the node in to to visit in correct place 		
+		if isSolution(currentNode):
+			path = []
+			while True:
+				path.append(currentNode) #appends the current node to the path-list. 
+				currentNode = currentNode.parent #changes currentNode to the parent
+
+				if currentNode == None: #the path is complete
+					return path
+
+		elif len(toVisit) == 0:
+			print 'no solution exist'
 			return []
-		currentPath = toVisit.pop()
-		currentNode = currentPath[len(currentPath)]
-		if currentNode is endnode:
-			return currentNode
+
+		currentNode = heapq.heappop(toVisit)
+
+def generateTree():
+	print 'er i generateTree-'
+	startnode = Node(None)
+	for permutation in itertools.permutations(startnode.numbers):
+		node = Node(startnode)
+		startnode.neighbours.append(node)
+		node.numbers = permutation
+
+	
+
+	return startnode
+
+	
+print findPath(generateTree(), isCorrectFraction)
 
 		
 
